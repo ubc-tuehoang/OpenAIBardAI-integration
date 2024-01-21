e12268e70a9b6e29f6c386e4eecec50324997217
ubc-tuehoang <86985864+ubc-tuehoang@users.noreply.github.com>
2024-01-20 16:18:33 -0800

    Update openbardai-4.py

--- openbardai-4.py
+++ openbardai-4.py
@@ -11,10 +11,8 @@ file_path = 'inputfile1.in'  # Replace 'path_to_your_file.txt' with your file pa
 file_contents = read_file(file_path)
 
 ##Secure-1PSID
-token = 'cAiYbRlzRcOtLci8ntX4DbABayA--9V9y8HKQj7T3JALHbJHz3CmcKv6PLmuA3mIYMRRCQ.'
-token = 'cAiYbRlzRcOtLci8ntX4DbABayA--9V9y8HKQj7T3JALHbJHz3CmcKv6PLmuA3mIYMRRCQ.'
-token = 'cAiYbRlzRcOtLci8ntX4DbABayA--9V9y8HKQj7T3JALHbJHz3CmcKv6PLmuA3mIYMRRCQ.'
-token = 'cwiYbbrS82T4J3T8ydjzpcec_YdwRuH7GJD5jHPkB2HrQqdVM-MxtjLRQchAkWnT7Xx6NQ.'
+token = 'BARD-AI Token'
+
 bard = Bard(token=token)
 #queryString = "PRIME WITH **Core courses for business*** COMM 101 - Business Fundamentals: grade above 90 then move on to COMM 291; grade between 85 and 90 student can move to COMM 291A; grade between 70 and 85 student can move to COMM 291B, else repeat COMM 101* COMM 291 - Application of Statistics in Business: grade above 90 then move on to COMM 292; grade between 85 and 90 student can move to COMM 292A; grade between 70 and 85 student can move to COMM 292B, else repeat COMM 291* COMM 291A - Application of Statistics in Business* COMM 291B - Application of Statistics in Business* COMM 292 - Management and Organizational Behaviour: grade above 70 then move on to ECON 101 or MATH 104; else repeat COMM 291* COMM 292A - Application of Statistics in Business * COMM 292B - Application of Statistics in Business * ECON 101 - Principles of Microeconomics * MATH 104 - Differential Calculus with Applications to Commerce and Social Sciences * OVERALL average of COMM 101, COMM 291 and COMM 292 between 75 and 90, the student can only take MINOR of ECON * OVERALL average of COMM 101, COMM 291 and COMM 292 above 90, the student can move to a quantitative MAJOR in sauder such as Accounting, Finance, BTM, Supply chain and logistics" 
 queryString = file_contents
@@ -46,7 +44,7 @@ payload = json.dumps({
 })
 headers = {
   'Content-Type': 'application/json',
-  'Authorization': 'Bearer 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'
+  'Authorization': 'Bearer <OPENAI Token>'
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
